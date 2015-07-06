754456a2be96ef96b959b6e68150f019034f616c
James Saint <james@jamessaint.com>
2015-07-06 14:55:36 +0100

    full-pack

---
+++ assets/plugins/fraud-loss-countup/fraud-loss-countup.js
@@ -0,0 +1,77 @@
+/* Copyright 2015 James Saint
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.licences.jamessaint.com/money-count-up
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+var startDate;
+var endDate;
+
+$(document).ready(function() {
+    function UpdateDollarz(){
+        startDate = new Date('1/1/2015 00:00:01 AM GMT').getTime();
+        endDate = new Date().getTime(); // ALWAYS THE CURRENT, PRECISE TIME...
+
+        // FOR A SPAN ITS NOT .VAL() ... INSTEAD ITS .TEXT() OR .HTML()...
+        // YOUR VERSION WAS ALSO MISSING THE MYDIFF FUNCTION.  
+        // USING THE CONSOLE TO WATCH FOR ERRORS WILL GIVE YOU CLUES 
+        // ON WHERE THE ISSUES ARE WHEN DEBUGGING JAVASCRIPT.
+        
+        $('#dollar-figure').text('$' + (mydiff(startDate, endDate, 'seconds') * 357).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"));
+    };
+    
+    setInterval(function(){
+        UpdateDollarz();
+    }, 1000);
+
+});
+
+// GENERIC FUNCTION TO CALCULATE THE SPAN OF TIME BETWEEN TWO JAVASCRIPT DATE OBJECTS...
+function mydiff(date1, date2, interval) {
+    console.log('mydiff function called...');
+    console.log('start date: ' + startDate);
+    console.log('end date: ' + endDate);
+
+    var second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24,
+        week = day * 7;
+    //date1 = new Date(date1); // PREVIOUSLY IT JUST USED HARD DATES, NOT PRECISE TIME STAMPS.
+    //date2 = new Date(date2); // PREVIOUSLY IT JUST USED HARD DATES, NOT PRECISE TIME STAMPS.;
+    date1 = new Date(date1).getTime(); // NOW WE USE TIMESTAMP FOR START... 
+    date2 = new Date().getTime(); // AND UP TO DATE TIMESTAMPS FOR THE END DATE
+    var timediff = date2 - date1;
+    if (isNaN(timediff)) return NaN;
+    switch (interval) {
+        case "years":
+            return date2.getFullYear() - date1.getFullYear();
+        case "months":
+            return (
+                (date2.getFullYear() * 12 + date2.getMonth()) -
+                (date1.getFullYear() * 12 + date1.getMonth())
+            );
+        case "weeks":
+            return Math.floor(timediff / week);
+        case "days":
+            return Math.floor(timediff / day);
+        case "hours":
+            return Math.floor(timediff / hour);
+        case "minutes":
+            return Math.floor(timediff / minute);
+        case "seconds":
+            return Math.floor(timediff / second);
+        default:
+            return undefined;
+    };
+};
--- index.php
+++ index.php
@@ -1,23 +1,82 @@
+
+
 <!DOCTYPE html>
 <html lang="en_GB">
 <head>
+
+<!-- ==============================================
+Internet Explorer
+=============================================== -->
+
+<!--[if lt IE 9]>
+<script type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script type="text/javascript" src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!--[if lt IE 8]>
+<script type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script type="text/javascript" src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!--[if lt IE 7]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
+<![endif]-->
+
+<!--[if lt IE 6]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
+<![endif]-->
+
+<!-- ==============================================
+Titles and Meta Tags
+=============================================== -->
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<title>Money Loss Count-UP</title>
+
+<!-- ==============================================
+CSS Elements
+=============================================== -->
+
+<link rel="stylesheet" href="assets/plugins/normalize/normalize.min.css">
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+
+
 </head>
 <body>
 
-    <span id="number"> <?=(int)((time()-15595199)/357.13);?></span>
+<section class="section-white">
+    
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="help-us">
+                            <div class="col-md-6 col-md-offset-3">
+                                <h2 class="counter-heading">Money Count Up</h2>
+                                    <div class="counter">
+                                        <span id="dollar"><sup class="counter-sup">*</sup><span id="dollar-figure"></span>
 
-    <script type="text/javascript">
+                                        <p class="small-print"><sup>*</sup> "Estimated money Loss / Gain"</p>
+                                    </div> <!-- /.fraud-counter -->
 
-     var i = <?=(int)((time()-15595199)/1);?>;
-     function increment() {
-     i++;
-     document.getElementById('number').innerHTML = i;
-     }
-     function numberWithCommas(i) {
-     return i.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-     }
-     setInterval('increment()', 1);
-    </script>
+                            </div> <!-- /.col-md-6 col-md-offset-3 -->
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+</section> <!-- /.section-white -->
+
+
+<!-- ==============================================
+jQuery and JavaScript Plugins
+=============================================== -->
+
+<script type="text/javascript" src="assets/plugins/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="assets/plugins/fraud-loss-countup/fraud-loss-countup.js"></script>
 
 </body>
 </html>
